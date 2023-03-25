b4da50d5acb9f9b93954c2b0c433d0e9628970b8
AndelysP <andelyst@hotmail.fr>
2023-03-25 23:03:16 +0100

    Ajout recherche selon prix / nb de personnes / type d'activité

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import { useEffect, useState } from 'react';
-import './App.css';
+import './assets/sass/App.scss';
+import Navbar from './components/Navbar';
 
 function App() {
 
-  const [ideas, setIdeas] = useState([]);
+  const [ideas, setIdeas] = useState([]); // Idée aléatoire
+
+  // Idée selon le type d'activité 
+  const [type, setType] = useState("")
+  const [typeIdeas, setTypeIdeas] = useState([]);
+
+  // Idée selon le nombre de personnes
+  const [numbers, setNumbers] = useState(0);
+  const [numberIdeas, setNumbersIdeas] = useState([]);
+
+  // Idée selon le prix 
+  const [price, setPrice] = useState(0);
+  const [priceIdeas, setPriceIdeas] = useState([]);
 
   const getIdeas = async () => {
     await fetch("http://www.boredapi.com/api/activity/")
@@ -20,11 +33,85 @@ function App() {
     getIdeas();
   };
 
+  const getTypeIdeas = async () => {
+    await fetch(`http://www.boredapi.com/api/activity?type=${type}`)
+      .then(res => res.json())
+      .then(data => setTypeIdeas(data))
+  }
+
+  const handleSubmitType = (e) => {
+    e.preventDefault();
+
+    getTypeIdeas();
+  }
+
+  const numberParticipantIdea = async () => {
+    await fetch(`http://www.boredapi.com/api/activity?participants=${numbers}`)
+      .then(res => res.json())
+      .then(data => setNumbersIdeas(data))
+  }
+
+
+  const handleSubmitNumber = (e) => {
+    e.preventDefault();
+
+    numberParticipantIdea();
+  }
+
+  const getPriceIdeas = async () => {
+    await fetch(`http://www.boredapi.com/api/activity?price=${price}`)
+      .then(res => res.json())
+      .then(data => setPriceIdeas(data))
+  }
+
+  const handleSubmitPrice = (e) => {
+    e.preventDefault();
+
+    getPriceIdeas();
+  }
+
+  console.log(priceIdeas);
+
   return (
     <div className="App">
-      <h1>Générateur d'idées :</h1>
-      <p>{ideas.activity}</p>
-      <button onClick={() => newIdea()}>Générer une nouvelle idée</button>
+      <Navbar />
+
+      <div className="generator">
+        <h1>Générateur d'idées :</h1>
+
+        <p>{ideas.activity}</p>
+        <button onClick={() => newIdea()} className="generator_btn">Générer une nouvelle idée</button>
+
+        <form onSubmit={handleSubmitType}>
+          <input type="text" value={type} onChange={(e) => setType(e.target.value)} />
+          <button type='submit'> Rechercher</button>
+        </form>
+
+        <ul>
+          <li>{typeIdeas.activity}</li>
+        </ul>
+
+        <form onSubmit={handleSubmitNumber}>
+          <input type="number" value={numbers} onChange={(e) => setNumbers(e.target.value)} />
+          <button type='Submit'>Nombre de personnes</button>
+        </form>
+
+        <ul>
+          <li>{numberIdeas.activity}</li>
+        </ul>
+
+        <form onSubmit={handleSubmitPrice}>
+          <input type="number" value={price} onChange={(e) => setPrice(e.target.value)} />
+          <button type='Submit'>Prix</button>
+        </form>
+
+        <p>La fourchette est comprise entre 0 et 1</p>
+        <ul>
+          <li>{priceIdeas.activity}</li>
+        </ul>
+      </div>
+
+
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../assets/img/logo.png';
+
+const Navbar = () => {
+  return (
+    <header>
+        <nav>
+            <img src={logo} alt="" />
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
